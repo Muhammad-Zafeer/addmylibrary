7149fbf708eaf6b9f00ca3e717648f8e2daf8a20
dell <zafeer.zb@gamil.com>
2021-12-15 10:15:36 +0500

    callback method add

--- app/mylibrary/src/main/java/com/example/mylibrary/add.java
+++ app/mylibrary/src/main/java/com/example/mylibrary/add.java
@@ -8,31 +8,24 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class add extends AppCompatActivity {
-    EditText e1,e2;
-    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add);
-        e1=findViewById(R.id.e1);
-        e2=findViewById(R.id.e2);
-        button=findViewById(R.id.b1);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                add();
-            }
-        });
+
     }
-    public int add()
+    public static void add(String e1,String e2, CallbackMethod callbackMethod)
     {
         int num1;
         int num2;
         int sum;
-       num1= Integer.parseInt(e2.getText().toString());
-       num2= Integer.parseInt(e1.getText().toString());
+       num1= Integer.parseInt(e2.toString());
+       num2= Integer.parseInt(e1.toString());
        sum=num1+num2;
-       return sum;
+       CallbackMethod callback = callbackMethod;
+       callback.add1(sum);
+    }
+    public interface CallbackMethod{
+        public void add1(int result);
     }
 }
--- app/src/main/java/com/example/addmylibrary/MainActivity.java
+++ app/src/main/java/com/example/addmylibrary/MainActivity.java
@@ -3,17 +3,39 @@ package com.example.addmylibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.mylibrary.add;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements add.CallbackMethod {
+
+    EditText e1,e2;
+    Button button;
+    add.CallbackMethod callbackMethod = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // mylibrary
+        e1=findViewById(R.id.e1);
+        e2=findViewById(R.id.e2);
+        button=findViewById(R.id.b1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                add.add(e1.getText().toString(),e2.getText().toString(),callbackMethod);
+            }
+        });
+    }
 
 
+    @Override
+    public void add1(int result) {
+        Toast.makeText(this, ""+result, Toast.LENGTH_SHORT).show();
     }
 }
